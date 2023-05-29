bebe1ce9d52c7d98ec949da1f8d40afe9cc4b2e8
Cameron Jones <camandrewjones@gmail.com>
2023-05-29 14:19:40 +0300

    Setting sun

---
+++ src/City.tsx
@@ -0,0 +1,7 @@
+import kaupunki from './assets/kaupunkinettiin.png'
+
+export function City() {
+    return <div className="city">
+        <img src={kaupunki} alt='city'/>
+    </div>
+}
---
+++ src/Sun.tsx
@@ -0,0 +1,9 @@
+import wonesyLogo from './assets/wonesylogo.png'
+
+export function Sun() {
+    return (
+        <div className="sun">
+            <img src={wonesyLogo} alt="wonesy logo" />
+        </div>
+    )
+}
